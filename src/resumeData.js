e95b9345499a9e673d862cc4918a0a88ba28c2a9
Patrick Follett <pwf592110@gmail.com>
2019-09-30 14:59:05 -0400

    react commiting

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/PWFollett",
+    "name": "Patrick Follett",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/patrickwfollett/",
+    "skypeid": "http://twitter.com/LaPat_Blue",
+    "roleDescription": " I enjoy working in various parts of frontend development and backend development! Whenever I have free time today I like to learn and try new things. I played volleyball all my life and still do to this day!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/patrickwfollett/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/PWFollett",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/LaPat_Blue",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a recent graduate of the University of Pennsylvania's LPS Coding BootCamp. Over the last six months I have been tested in skills both frontend and backend and have learn an amazing amount. I came into this program wanting to learn how to be a developer and I can say that I am confident in my skills as a Full Stack Developer. I am looking to work for a company that is focused on advancement and has a eye on the future. I am always thinking of creative and innovative ways to solve complex problems. I love to learn and can not wait to broaden my language base!",
+    "address":"Pittsburgh, PA",
+    "website":"https://github.com/PWFollett",
+    "education":[
+      {
+        "UniversityName":"University of Pennsylvania LPS Coding BootCamp",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2019",
+        "Achievements":"Graduated"
+      },
+      {
+        "UniversityName":"Ohio University",
+        "specialization":"Political Marketing and Strategic Communications",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":"Graduated 3.5/4.0"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"FedEx Services ",
+        "specialization":"Senior Account Exective",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2019",
+        "Achievements":"Ambassadors Club 2018"
+      },
+      {
+        "CompanyName":"PLS Logistics",
+        "specialization":"Third Party Logisitics Broker",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2016",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I worked with Patrick on several projects throughout our time studying online with the University of Pennsylvania's LPS Coding Bootcamp. Saying Patrick is a team player and a really great person to work with would be quite a heavy understatement. Patrick Follett has a clear drive to learn and hone his craft as a professional developer, he does everything he can to reach his goals and is always willing to stay up some nights to help reach MVP on projects during tedious time crunches with little time left to get the job done. He's a quick learner with a passion for coding, but also, he's a great guy with an awesome personality! I would highly, highly recommend Mr. Follett to any company. He's not just an asset to your team, but a wonderful co-worker and friend and is always willing to help with complex issues whenever you need it.",
+        "name":"Shawn Luther, Jr. Web Developer" 
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
